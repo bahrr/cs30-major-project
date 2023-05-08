e06b0b2c8000f7223cc5952a1f608801dbe91579
Ivan <kivaniovich@gmail.com>
2023-05-08 15:21:38 -0600

    began work on the map lumps

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,10 @@ struct  BspMap {
 
 // Holds onto raw lump data
 struct Lump {
+    name: String,
 
+    // A vector to store the raw data of thy lump
+    data: Vec<u8>,
 }
 // Things are 2d objects like monsters or items
 struct Thing {
@@ -58,7 +61,7 @@ impl Wad {
         let info_table = <LittleEndian as ByteOrder>::read_u32(&file[8..12]);
 
         let mut lumps: Vec<Lump> = Vec::new(); // Stores the raw lumps to go over in a list
-        let mut maps:Vec<BspMap> = Vec::new();
+        let mut maps:Vec<BspMap> = Vec::new(); // Stores the game maps
 
         // Appends the lump vector with lumps obtained from the WAD
         for i in 0..num_of_lumps {
@@ -72,10 +75,28 @@ impl Wad {
             let lump_size = <LittleEndian as ByteOrder>::read_u32(&file[dir_loc+4..dir_loc+8]) as usize;
             
             // The name of the lump
-            let lump_name = str::from_utf8(&file[dir_loc+8..dir_loc+16])
+            let lump_name = String::from_utf8(file[dir_loc+8..dir_loc+16].to_vec())
                 .unwrap();
             
-            let raw_bytes = &file[lump_pos..lump_pos + lump_size];
+            // The raw bytes of the lump as a vector
+            let raw_bytes = file[lump_pos..lump_pos + lump_size].to_vec();
+
+            lumps.push(Lump {name: lump_name, data: raw_bytes});
+        }
+        
+        // Goes over map lumps to convert into something usable for a renderer
+        let mut i = 0;
+        let mut lump_name = lumps[0].name.as_str();
+
+        while lump_name != "PLAYPAL\0" { // PLAYPAL with a null character happens to be the first non map lump in a WAD file
+            println!("{lump_name}");
+
+            let mut map_lumps: [Vec<u8>; 11];
+            for j in i..i+11 {
+                // TODO: make array of map lumps to make it easier for the function to work
+            }
+            i += 11;
+            lump_name = lumps[i].name.as_str();
         }
 
         Wad {
@@ -86,10 +107,15 @@ impl Wad {
     }
 }
 
+impl BspMap {
+    fn new(data: &[Vec<u8>]) -> BspMap {
+
+        BspMap { things: vec![] 
+         }
+    } 
+}
+
 
 fn main() {
-    let iwad = Wad::load("assets/freedoom1.wad");
-
-    // Just quickly reads out the IWAD's header
-    println!("{}", iwad.identification);
+    Wad::load("assets/freedoom1.wad");
 }
